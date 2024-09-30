e5709b0264e94f214d0e6febc47f2d8942a6faf3
Erika Santana <113842892+erikasntn@users.noreply.github.com>
2024-09-30 13:51:07 -0300

    Add files via upload

---
+++ SYP-Front/src/pages/Como-Funciona.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegistroSection from '../components/Como-Funciona/RegistroSection';
+
+
+function ComoFunciona() {
+  return (
+    <div>
+      <RegistroSection/>
+    </div>
+  );
+}
+
+export default ComoFunciona;
